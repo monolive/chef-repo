26b8925f2bfc20f1ed295ad9cfd150c4a117a4c5
Olivier Renault <olivier.renault@eucalyptus.com>
2012-12-11 22:36:39 +0000

    fix yum repository

--- cookbooks/percona/recipes/default.rb
+++ cookbooks/percona/recipes/default.rb
@@ -5,16 +5,23 @@
 # Copyright 2012, YOUR_COMPANY_NAME
 #
 
+include_recipe"yum::yum" 
+
+yum_key "RPM-GPG-KEY-percona" do 
+  url "http://www.percona.com/downloads/percona-release/RPM-GPG-KEY-percona" 
+  action :add 
+end
+
 yum_repository "percona" do
   description "Percona RPM repository"
   url "http://repo.percona.com/centos/$releasever/os/$basearch/"
-  key "http://www.percona.com/redir/downloads/percona-release/RPM-GPG-KEY-percona"
+  key "RPM-GPG-KEY-percona"
   action :add
 end
 
 yum_repository "percona-testing" do
   description "Percona testing RPM Repository"
   url "http://repo.percona.com/testing/centos/$releasever/os/$basearch/"
-  key "http://www.percona.com/redir/downloads/percona-release/RPM-GPG-KEY-percona"
+  key "RPM-GPG-KEY-percona"
   action :add
 end
