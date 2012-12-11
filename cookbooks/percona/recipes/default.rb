464307f5d6c80329cf37ee7baf0e3abd74353af8
Olivier Renault <monoliv@gmail.com>
2012-12-11 17:27:27 +0000

    adding yum repository

--- cookbooks/percona/recipes/default.rb
+++ cookbooks/percona/recipes/default.rb
@@ -4,3 +4,17 @@
 #
 # Copyright 2012, YOUR_COMPANY_NAME
 #
+
+yum_repository "percona" do
+  description "Percona RPM repository"
+  url "http://repo.percona.com/centos/$releasever/os/$basearch/"
+  key "http://www.percona.com/redir/downloads/percona-release/RPM-GPG-KEY-percona"
+  action :add
+end
+
+yum_repository "percona-testing" do
+  description "Percona testing RPM Repository"
+  url "http://repo.percona.com/testing/centos/$releasever/os/$basearch/"
+  key "http://www.percona.com/redir/downloads/percona-release/RPM-GPG-KEY-percona"
+  action :add
+end
