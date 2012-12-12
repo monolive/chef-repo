ca9ae012682afabeea9b25d5eb9a1ea12177078a
Olivier Renault <monoliv@gmail.com>
2012-12-12 12:24:12 +0000

    modify variable name

--- cookbooks/percona/attributes/defaults.rb
+++ cookbooks/percona/attributes/defaults.rb
@@ -6,5 +6,5 @@
 
 default['percona']['key_url'] = "http://www.percona.com/redir/downloads/percona-release/RPM-GPG-KEY-percona"
 default['percona']['key'] = "RPM-GPG-KEY-percona"
-default['percona']['url_release'] = "http://repo.percona.com/centos/$releasever/os/$basearch/"
-default['percona']['url_testing'] = "http://repo.percona.com/testing/centos/$releasever/os/$basearch/"
+default['percona']['release_url'] = "http://repo.percona.com/centos/$releasever/os/$basearch/"
+default['percona']['testing_url'] = "http://repo.percona.com/testing/centos/$releasever/os/$basearch/"
