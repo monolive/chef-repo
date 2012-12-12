9e6fcbdcac89bc740593ae00fad1fe79e5387b10
Olivier Renault <monoliv@gmail.com>
2012-12-12 22:44:21 +0000

    add extra output

--- cookbooks/percona/recipes/default.rb
+++ cookbooks/percona/recipes/default.rb
@@ -25,8 +25,8 @@ when "redhat", "centos"
 end
 
 # Create list of server which are using percona recipe and sharing clustername
-cluster_members = search("node", "recipe:percona AND node:#{node['percona']['cluster_name']}")
-
+cluster_members = search("node", "role:percona AND node:#{node['percona']['cluster_name']}")
+toto = search("node", "role:percona")
 # remove mysql-libs as it conflict w/ percona server pkg
 # it will also remove redhat-lsb as a dependency
 # we will reinstall it afterward
