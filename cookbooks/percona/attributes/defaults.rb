88903fe0a05e1e3073233a258a77f61b9f522145
Olivier Renault <monoliv@gmail.com>
2012-12-12 13:34:48 +0000

    add comment

--- cookbooks/percona/attributes/defaults.rb
+++ cookbooks/percona/attributes/defaults.rb
@@ -4,6 +4,7 @@
 # Attributes:: default
 #
 
+# Repository information - GPG key information are unused
 default['percona']['key_url'] = "http://www.percona.com/redir/downloads/percona-release/RPM-GPG-KEY-percona"
 default['percona']['key'] = "RPM-GPG-KEY-percona"
 default['percona']['release_url'] = "http://repo.percona.com/centos/$releasever/os/$basearch/"
