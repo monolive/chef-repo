6aa31be7daa0c766890b9849ac515cf1c48cb278
Olivier Renault <monoliv@gmail.com>
2012-12-12 14:28:56 +0000

    add mysql user

--- cookbooks/percona/attributes/defaults.rb
+++ cookbooks/percona/attributes/defaults.rb
@@ -4,8 +4,14 @@
 # Attributes:: default
 #
 
-# Repository information - GPG key information are unused
+# Repository information
+# GPG key information are currently unused
 default['percona']['key_url'] = "http://www.percona.com/redir/downloads/percona-release/RPM-GPG-KEY-percona"
 default['percona']['key'] = "RPM-GPG-KEY-percona"
 default['percona']['release_url'] = "http://repo.percona.com/centos/$releasever/os/$basearch/"
 default['percona']['testing_url'] = "http://repo.percona.com/testing/centos/$releasever/os/$basearch/"
+
+# Configuration
+default['percona']['mysql_data'] = "/percona/data"
+default['percona']['mysql_user'] = "mysql"
+
