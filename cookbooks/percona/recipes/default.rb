e5520bd152bbb6b69134bf02d33a2006686292aa
Olivier Renault <monoliv@gmail.com>
2012-12-12 13:33:17 +0000

    remove gpg checking, add platform check

--- cookbooks/percona/recipes/default.rb
+++ cookbooks/percona/recipes/default.rb
@@ -4,29 +4,22 @@
 #
 # Copyright 2012, YOUR_COMPANY_NAME
 #
+# TODO : Need to use the gpg keys
 
-include_recipe"yum::yum" 
-
-key         = node['percona']['key']
-key_url     = node['percona']['key_url']
 release_url = node['percona']['release_url']
 testing_url = node['percona']['testing_url']
 
-yum_key "#{key}" do 
-  url "#{key_url}"
-  action :add 
-end
+case node[:platform]
+when "redhat", "centos"
+  yum_repository "percona" do
+    description "Percona RPM repository"
+    url release_url
+    action :add
+  end
 
-yum_repository "percona" do
-  description "Percona RPM repository"
-  url "#{release_url}"
-  key "#{key}"
-  action :add
-end
-
-yum_repository "percona-testing" do
-  description "Percona testing RPM Repository"
-  url "#{testing_url}"
-  key "#{key}"
-  action :add
+  yum_repository "percona-testing" do
+    description "Percona testing RPM Repository"
+    url testing_url
+    action :add
+  end
 end
